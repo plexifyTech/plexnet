df6d9fad456a14bc7ad221831b23e3ddca6ecf0f
Philipp Kang <philipp.kang@plexify.io>
2022-11-03 12:00:53 +0100

    added function to load config

--- config.go
+++ config.go
@@ -1,7 +1,19 @@
 package plexnet
 
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+)
+
 type AppConfig struct {
 	Port     string `yaml:"port" env:"PORT"`
 	AppName  string `yaml:"appName" env:"APP_NAME"`
 	BasePath string `yaml:"basePath"`
+}
+
+func LoadConfig(config interface{}) {
+	err := cleanenv.ReadConfig("config.yaml", &config)
+	if err != nil {
+		log.Fatalf("error reading config file %v", err)
+	}
 }
